18f86ba9ea1ef89ee25d5bd355fcf87dec055b37
Saman Tavasoli <samsamtava@gmail.com>
2024-09-13 21:18:45 -0700

    some changes

--- blockchain.py
+++ blockchain.py
@@ -3,17 +3,23 @@ blockchain = []
 def get_last_blockchain_value():
     return blockchain[-1]
 
-
 def add_value(transaction_amount, last_transaction = [1]):
     blockchain.append([last_transaction, transaction_amount])
+
+def get_user_input():
+    return float(input("Your transaction amount please: "))
     
-tx_amount = float(input("Your transaction amount please: "))
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input("Your transaction amount please: "))
-add_value(tx_amount)
-add_value(last_transaction= get_last_blockchain_value(), transaction_amount= 0.9)
-add_value(10.89, get_last_blockchain_value())
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+
 
 print(blockchain)
 
